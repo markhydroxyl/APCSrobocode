a7935f3224ae4ef98e57eccae6e313ea32ecfbfd
Felix Yu <felixyu2001@gmail.com>
2017-09-30 22:44:23 -0400

    Even better version, modified linear targeting, changed firePower, changed fraction

--- CrcTrgtMinRskBltShldLstScn.java
+++ CrcTrgtMinRskBltShldLstScn.java
@@ -13,7 +13,7 @@ public class CrcTrgtMinRskBltShldLstScn extends AdvancedRobot {
 	static double scanDir;
 	static Object sought;
 	double curX, curY, nextX, nextY, lastX, lastY;
-	double curNRG;
+	double curNRG, curTime;
 	double fieldX, fieldY;
 	double distToTarget;
 	
@@ -30,40 +30,72 @@ public class CrcTrgtMinRskBltShldLstScn extends AdvancedRobot {
 			curX = getX();
 			curY = getY();
 			curNRG = getEnergy();
+			curTime = getTime();
 			setTurnRadarRightRadians(scanDir * Double.POSITIVE_INFINITY);
 			if (target != null) {
 				distToTarget = Math.hypot(target.enX-curX, target.enY-curY);
 				shoot();
 			}
-			if (getTime() > 9)
+			if (curTime > 9)
 				move();
 			execute();
 		}
 	}
 	
 	public void shoot() {
-		double firePower = Math.min(target.enNRG/3, Math.min(curNRG/6, 1300/target.enDist));
+		System.out.println(target.enName);
+		if (curTime > target.lastScanned + 9) {
+			target = null;
+			return;
+		}
+		System.out.println(target.enName);
+		double fraction;
+		double lowBound = 50;
+		double highBound = 1000;
+		if (target.enDist > highBound)
+			fraction = 0;
+		else if (target.enDist < lowBound)
+			fraction = 1;
+		else
+			fraction = (target.enDist-lowBound)/(highBound-lowBound);
+		double firePower = Math.min(3, target.enNRG);
+		double bVelocity = 20-3*firePower;
 		if (getGunTurnRemaining() == 0 && curNRG > firePower) {
 		    setFire(firePower);
 		}
-		setTurnGunRightRadians(Utils.normalRelativeAngle(calcAngle(curX, curY, target.enX, target.enY) - getGunHeadingRadians()));
-		//Head-on targeting
+		double ang = (Utils.normalRelativeAngle(target.enHeading - getGunHeadingRadians() + Math.asin((target.enVel * Math.sin(target.eventHeading - target.enHeading) / bVelocity)))*fraction);
+		setTurnGunRightRadians(ang);
+		System.out.println(ang);
+		//Modified linear targeting
 	}
 	
 	public void move() {
 		double distToDest = Math.hypot(nextX-curX, nextY-curY);
-		if (distToDest < 15) {
+		if (distToDest < 50) {
 			double[] testPoint;
 			double lowestRisk = Double.MAX_VALUE;
-			for (int i=0; i<100; i++) {
-				testPoint = pickPoint(curX, curY, 2*Math.PI*Math.random(), 200+Math.random()*400);
-				System.out.println(testPoint[0]+" "+testPoint[1]);
-				if (testPoint[0] > 30 && testPoint[0] < fieldX-30 && testPoint[1] > 30 && testPoint[1] < fieldY-30) {
-					double curRisk = calcRisk(testPoint);
-					if (curRisk < lowestRisk) {
-						nextX = testPoint[0];
-						nextY = testPoint[1];
-						lowestRisk = curRisk;
+			for (int i=0; i<500; i++) {
+				if (target != null) {
+					testPoint = pickPoint(curX, curY, 2*Math.PI*Math.random(), Math.min(0.9*target.enDist, 200+Math.random()*400));
+					System.out.println(testPoint[0]+" "+testPoint[1]);
+					if (testPoint[0] > 30 && testPoint[0] < fieldX-30 && testPoint[1] > 30 && testPoint[1] < fieldY-30) {
+						double curRisk = calcRisk(testPoint);
+						if (curRisk < lowestRisk) {
+							nextX = testPoint[0];
+							nextY = testPoint[1];
+							lowestRisk = curRisk;
+						}
+					}
+				} else {
+					testPoint = pickPoint(curX, curY, 2*Math.PI*Math.random(), 400+Math.random()*400);
+					System.out.println(testPoint[0]+" "+testPoint[1]);
+					if (testPoint[0] > 30 && testPoint[0] < fieldX-30 && testPoint[1] > 30 && testPoint[1] < fieldY-30) {
+						double curRisk = calcRisk(testPoint);
+						if (curRisk < lowestRisk) {
+							nextX = testPoint[0];
+							nextY = testPoint[1];
+							lowestRisk = curRisk;
+						}
 					}
 				}
 			}
@@ -82,6 +114,7 @@ public class CrcTrgtMinRskBltShldLstScn extends AdvancedRobot {
 			setAhead(distToDest*dir);
 			setTurnRightRadians(ang = Utils.normalRelativeAngle(ang));
 		}
+		//Minimum risk movement
 	}
 	
 	public void onScannedRobot(ScannedRobotEvent e) {
@@ -93,12 +126,16 @@ public class CrcTrgtMinRskBltShldLstScn extends AdvancedRobot {
 			justScanned.enDist = e.getDistance();
 			justScanned.enNRG = e.getEnergy();
 			justScanned.enHeading = e.getBearingRadians() + getHeadingRadians();
+			justScanned.enVel = e.getVelocity();
+			justScanned.eventHeading = e.getHeading();
 			justScanned.enX = curX + Math.sin(justScanned.enHeading)*justScanned.enDist;
 			justScanned.enY = curY + Math.cos(justScanned.enHeading)*justScanned.enDist;
+			justScanned.lastScanned = curTime;
 		} else {
-			enHashtable.put(name, new enemy(e.getEnergy(), e.getDistance(), e.getBearingRadians() + getHeadingRadians()));
+			enHashtable.put(name, new enemy(e.getEnergy(), e.getDistance(), e.getBearingRadians() + getHeadingRadians(), e.getVelocity(), e.getHeading(), getTime()));
 			justScanned = enHashtable.get(name);
 			target = justScanned;
+			justScanned.enName = name;
 		}
 	    
 	    if ((name == sought || sought == null) && enHashtable.size() == getOthers()) {
@@ -106,8 +143,18 @@ public class CrcTrgtMinRskBltShldLstScn extends AdvancedRobot {
 	    	sought = enHashtable.keys().nextElement();
 	    }
 	    
-	    if (target == null)
-	    	target = justScanned;
+	    if (target == null) {
+	    	double lowNRG = Double.MAX_VALUE;
+	    	String tarName = "";
+	    	for (int i=0; i<enHashtable.size(); i++) {
+	    		enemy nextEn = enHashtable.values().iterator().next();
+	    		if (nextEn.enNRG < lowNRG) {
+	    			lowNRG = nextEn.enNRG; 
+	    			tarName = nextEn.enName;
+	    		}
+	    	}
+	    	target = enHashtable.get(tarName);
+	    }
 	    //Radar turning code derived from Oldest Scanned method found on Robowiki: http://robowiki.net/wiki/Melee_Radar#Oldest_Scanned
 	}
 	
@@ -123,8 +170,12 @@ public class CrcTrgtMinRskBltShldLstScn extends AdvancedRobot {
 		
 		while(enEnum.hasMoreElements()) {
 			enemy nextEn = (enemy) enEnum.nextElement();
-			dangFactor += nextEn.enNRG*(1+Math.abs(Math.cos(calcAngle(coor[0], coor[1], curX, curY)-calcAngle(coor[0], coor[1], target.enX, target.enY))))/Math.pow(nextEn.enDist, 2.5);
+			if (target != null)
+				dangFactor += Math.min(nextEn.enNRG, 2)*(1+Math.abs(Math.cos(calcAngle(coor[0], coor[1], curX, curY)-calcAngle(coor[0], coor[1], target.enX, target.enY))))/Math.pow(nextEn.enDist, 2);
+			else
+				dangFactor += nextEn.enNRG/Math.pow(nextEn.enDist, 2);
 		}
+		
 		
 		return dangFactor;
 	}
@@ -145,10 +196,17 @@ public class CrcTrgtMinRskBltShldLstScn extends AdvancedRobot {
 		public double enNRG;
 		public double enDist;
 		public double enHeading;
-		public enemy(double aNRG, double aDist, double aHeading) {
+		public double enVel;
+		public double eventHeading;
+		public String enName;
+		public double lastScanned;
+		public enemy(double aNRG, double aDist, double aHeading, double aVel, double aEHeading, double aScanned) {
 			enNRG = aNRG;
 			enDist = aDist;
 			enHeading = aHeading;
+			enVel = aVel;
+			eventHeading = aEHeading;
+			lastScanned = aScanned;
 			enX = curX + Math.sin(aHeading)*aDist;
 			enY = curY + Math.cos(aHeading)*aDist;
 		}
